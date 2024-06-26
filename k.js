809f5284abac481eee5ae673f5a17c0dd37b90fe
Simran THakur <131333146+tsimran-gmailcom@users.noreply.github.com>
2024-06-26 21:57:50 +0530

    Add files via upload

---
+++ k.js
@@ -0,0 +1,32 @@
+const logDiv =document.getElementById("log");
+const stateDiv =document.getElementById("state");
+const startBtn =document.getElementById("start-btn");
+const stopBtn =document.getElementById("stop-btn");
+
+
+startBtn.addEventListener("click", ()=>{
+    document.addEventListener("keydown",handleDown)
+    document.addEventListener("keyup",handleUp)
+    startBtn.disabled=true;
+    stopBtn.disabled=false;
+
+})
+
+stopBtn.addEventListener("click", ()=>{
+    document.removeEventListener("keydown",handleDown)
+    document.removeEventListener("keyup",handleUp)
+    logDiv.textContent="";
+    stateDiv.textContent="";
+    stopBtn.disabled=true;
+    startBtn.disabled=false;
+    
+})
+
+function handleDown(e){
+    logDiv.textContent = `key ${e.key} pressed down `;
+    stateDiv.textContent ="key is Down";
+}
+function handleUp(e){
+    logDiv.textContent=`key ${e.key} released `;
+    stateDiv.textContent="key is Up";
+}
